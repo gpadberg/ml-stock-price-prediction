5d0a8c5d8b541d44e5598eb9a8aa6f91f7c55741
gpadberg <144191036+gpadberg@users.noreply.github.com>
2024-06-20 17:13:45 +0000

    Added lstm split

--- project.py
+++ project.py
@@ -2,7 +2,16 @@ import pandas as pd
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import datetime as dt
-
+import numpy as np
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, LSTM, Dropout
+from tensorflow.python.keras.callbacks import EarlyStopping
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.metrics import mean_squared_error
 
 stock_data = pd.read_csv('./NFLX.csv', index_col = 'Date')
 stock_data.head()
@@ -21,6 +30,22 @@ plt.legend()
 plt.gcf().autofmt_xdate()
 plt.show()
 
-# Save the plot to a file
+# save the plot to a file
 plt.savefig('stock_plot.png')
 print("Plot saved as 'stock_plot.png'. Open this file to view the plot.")
+
+target_y = stock_data['Close']
+X_feat = stock_data.iloc[:, 0:3]
+
+# feature scaling
+sc = StandardScaler()
+X_ft = sc.fit_transform(X_feat.values)
+X_ft = pd.DataFrame(columns = X_feat.columns, data = X_ft, index = X_feat.index)
+
+def lstm_split(data, n_steps):
+    X, y = [], []
+    for i in range(len(data) - n_steps + 1):
+        X.append(data[i:i + n_steps, :-1])
+        y.append(data[i + n_steps - 1, -1])
+    return np.array(X), np.array(y)
+        
